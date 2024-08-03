b6ac8b7da3bcc668f398040973b31ca45438e7ff
abdomohamed96 <abdomohamed9192@gmail.com>
2024-08-03 20:29:52 +0300

    Add mongodb using docker-compose

--- dest/index.js
+++ dest/index.js
@@ -5,8 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = void 0;
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const node_path_1 = __importDefault(require("node:path"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
+dotenv_1.default.config({ path: node_path_1.default.resolve(__dirname, '../.env') });
+mongoose_1.default
+    .connect(process.env.MONGO_URI || 'error')
+    .then(() => console.log("connected to monogodb..."))
+    .catch((err) => console.log("error", err));
 app.get("/", (req, res) => {
     res.status(200).json({
         msg: "get started"
@@ -14,7 +22,7 @@ app.get("/", (req, res) => {
 });
 app.use('/admins', routes_1.adminRoute);
 app.use('/vendors', routes_1.vendorRoute);
-const PORT = 3000;
+const PORT = 3001;
 const server = app.listen(PORT, () => {
     console.log(`server run on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from "mongoose";
+
 import {Request,Response,NextFunction} from "express";
 import {adminRoute,vendorRoute} from './routes';
 const app=express();
+
+import dotenv from 'dotenv';
+import path from "node:path";
+
+
+dotenv.config({ path: path.resolve(__dirname, '../config.env') });
+
+mongoose
+    .connect(process.env.MONGO_URI || 'error')
+    .then(()=>console.log("connected to monogodb..."))
+    .catch((err)=>console.log("error",err))
 
 app.get("/",(req:Request,res:Response)=>{
     res.status(200).json({
@@ -11,7 +24,7 @@ app.get("/",(req:Request,res:Response)=>{
 
 app.use('/admins',adminRoute);
 app.use('/vendors',vendorRoute);
-const PORT=3000
+const PORT=3001
 const server=app.listen(PORT,()=>{
     console.log(`server run on port ${PORT}`)
 });
